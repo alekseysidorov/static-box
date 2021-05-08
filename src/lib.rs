1a2d9f6d9fb21366c911da0cd08f7d9377f5c41f
Aleksei Sidorov <gorthauer87@yandex.ru>
2021-05-08 13:17:10 +0300

    Refine static-box
    - Make it possible to use a user-provided buffer as a backend for the static box.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@
 use core::{
     alloc::Layout,
     marker::{PhantomData, Unsize},
-    mem::MaybeUninit,
     ops::{Deref, DerefMut},
     ptr::{self, drop_in_place, metadata, DynMetadata, NonNull, Pointee},
 };
@@ -13,21 +12,26 @@ use core::{
 #[cfg(test)]
 mod tests;
 
-// 
+//
 
-pub struct Box<T, const N: usize>
+pub struct Box<T, M>
 where
     T: ?Sized + Pointee<Metadata = DynMetadata<T>>,
+    M: AsRef<[u8]> + AsMut<[u8]>,
 {
-    mem: [u8; N],
+    mem: M,
     phantom: PhantomData<T>,
 }
 
-impl<T, const N: usize> Box<T, N>
+impl<T, M> Box<T, M>
 where
     T: ?Sized + Pointee<Metadata = DynMetadata<T>>,
+    M: AsRef<[u8]> + AsMut<[u8]>,
 {
-    pub fn new<Value: Unsize<T>>(value: Value) -> Self {
+    pub fn new_in_buf<Value>(mut mem: M, value: Value) -> Self
+    where
+        Value: Unsize<T>,
+    {
         let meta = metadata(&value as &T);
 
         let meta_layout = Layout::for_value(&meta);
@@ -35,11 +39,10 @@ where
         let (layout, offset) = meta_layout.extend(value_layout).unwrap();
         // TODO
         assert!(layout.size() > 0);
-        assert!(layout.size() <= N);
+        assert!(layout.size() <= mem.as_ref().len());
 
         unsafe {
-            let mut mem = MaybeUninit::uninit();
-            let ptr = NonNull::new(mem.as_mut_ptr()).unwrap();
+            let ptr = NonNull::new(mem.as_mut().as_mut_ptr()).unwrap();
             // TODO
             ptr.cast::<DynMetadata<T>>().as_ptr().write(meta);
             // TODO
@@ -50,14 +53,14 @@ where
                 .write(value);
 
             Self {
-                mem: mem.assume_init(),
+                mem,
                 phantom: PhantomData,
             }
         }
     }
 
     fn meta(&self) -> DynMetadata<T> {
-        unsafe { *self.mem.as_ptr().cast() }
+        unsafe { *self.mem.as_ref().as_ptr().cast() }
     }
 
     fn layout_meta(&self) -> (Layout, usize, DynMetadata<T>) {
@@ -69,7 +72,7 @@ where
     fn value_ptr(&self) -> *const T {
         let (_, offset, meta) = self.layout_meta();
         unsafe {
-            let ptr = self.mem.as_ptr().add(offset).cast::<()>();
+            let ptr = self.mem.as_ref().as_ptr().add(offset).cast::<()>();
             ptr::from_raw_parts(ptr, meta)
         }
     }
@@ -77,35 +80,71 @@ where
     fn value_mut_ptr(&mut self) -> *mut T {
         let (_, offset, meta) = self.layout_meta();
         unsafe {
-            let ptr = self.mem.as_mut_ptr().add(offset).cast::<()>();
+            let ptr = self.mem.as_mut().as_mut_ptr().add(offset).cast::<()>();
             ptr::from_raw_parts_mut(ptr, meta)
         }
     }
 }
 
-impl<T, const N: usize> Deref for Box<T, N>
+impl<T, const N: usize> Box<T, [u8; N]>
 where
     T: ?Sized + Pointee<Metadata = DynMetadata<T>>,
 {
-    type Target = T;
+    pub fn new<Value>(value: Value) -> Self
+    where
+        Value: Unsize<T>,
+    {
+        // TODO Investigate if it is possible to safely use an uninitialized memory buffer here.
+        Self::new_in_buf([0_u8; N], value)
+    }
+}
 
-    fn deref(&self) -> &T {
+impl<T, M> AsRef<T> for Box<T, M>
+where
+    T: ?Sized + Pointee<Metadata = DynMetadata<T>>,
+    M: AsRef<[u8]> + AsMut<[u8]>,
+{
+    fn as_ref(&self) -> &T {
         unsafe { &*self.value_ptr() }
     }
 }
 
-impl<T, const N: usize> DerefMut for Box<T, N>
+impl<T, M> AsMut<T> for Box<T, M>
 where
     T: ?Sized + Pointee<Metadata = DynMetadata<T>>,
+    M: AsRef<[u8]> + AsMut<[u8]>,
 {
-    fn deref_mut(&mut self) -> &mut T {
+    fn as_mut(&mut self) -> &mut T {
         unsafe { &mut *self.value_mut_ptr() }
     }
 }
 
-impl<T, const N: usize> Drop for Box<T, N>
+impl<T, M> Deref for Box<T, M>
 where
     T: ?Sized + Pointee<Metadata = DynMetadata<T>>,
+    M: AsRef<[u8]> + AsMut<[u8]>,
+{
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.as_ref()
+    }
+}
+
+impl<T, M> DerefMut for Box<T, M>
+where
+    T: ?Sized + Pointee<Metadata = DynMetadata<T>>,
+    M: AsRef<[u8]> + AsMut<[u8]>,
+{
+    fn deref_mut(&mut self) -> &mut T {
+        self.as_mut()
+    }
+}
+
+impl<T, M> Drop for Box<T, M>
+where
+    T: ?Sized + Pointee<Metadata = DynMetadata<T>>,
+    M: AsRef<[u8]> + AsMut<[u8]>,
 {
     fn drop(&mut self) {
         unsafe {
--- src/tests.rs
+++ src/tests.rs
@@ -7,28 +7,28 @@ use crate::Box;
 
 #[test]
 fn test_box_trait_object() {
-    let four = Box::<dyn Display, 32>::new(4);
+    let four = Box::<dyn Display, [u8; 32]>::new(4);
     assert_eq!(four.to_string(), "4");
     drop(four);
 
-    let seven = Box::<dyn Display, 32>::new(7);
+    let seven = Box::<dyn Display, [u8; 32]>::new(7);
     assert_eq!(seven.to_string(), "7");
 }
 
 #[test]
 fn test_box_move() {
-    fn move_me(b: Box<dyn Display, 32>) {
+    fn move_me(b: Box<dyn Display, [u8; 32]>) {
         assert_eq!(b.to_string(), "4");
     }
 
-    let b = Box::<dyn Display, 32>::new(4);
+    let b = Box::<dyn Display, [u8; 32]>::new(4);
     move_me(b);
 }
 
 #[test]
 #[should_panic]
 fn test_box_insufficient_memory() {
-    let _four = Box::<dyn Display, 2>::new(4);
+    let _four = Box::<dyn Display, [u8; 2]>::new(4);
 }
 
 #[test]
@@ -45,8 +45,16 @@ fn test_drop() {
     }
 
     let (tx, rx) = mpsc::channel();
-    let b = Box::<dyn Debug, 32>::new(Foo { tx });
+    let b = Box::<dyn Debug, [u8; 32]>::new(Foo { tx });
     drop(b);
 
     assert_eq!(rx.recv().unwrap(), 42);
+}
+
+#[test]
+fn test_box_in_provided_memory() {
+    let mut mem = [0_u8; 32];
+
+    let val: Box<dyn Display, _> = Box::new_in_buf(&mut mem, 42);
+    assert_eq!(val.to_string(), "42");
 }
