deb4affcb7d20e27c982e1153dba9525d5e7a9d6
Aleksei Sidorov <gorthauer87@yandex.ru>
2021-07-13 19:14:39 +0300

    Remove obsolete comment

--- src/lib.rs
+++ src/lib.rs
@@ -154,7 +154,6 @@ where
         assert!(layout.size() > 0, "Unsupported value layot");
 
         // Construct a box to move the specified memory into the necessary location.
-        // SAFETY: This code relies on the fact that this method will be inlined.
         let mut new_box = Self {
             align_offset: 0,
             mem,
